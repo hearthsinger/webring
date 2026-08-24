a674c1864860e8754b621bb57ab939b758df06de
Asteria Hart <asteria@strawbs.io>
2026-08-24 19:32:24 -0400

    chore: tweak permissions, docs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 'use strict';
+/**
+ * @file Main entrypoint for the webring
+ * @author Asteria Hart <asteria@strawbs.io>
+ */
 import express from 'express';
 import {RING_MEMBER_DEFINITIONS} from './ring.js';
 import { RingError, NotFoundError, BadRequestError, ErrorCode } from './errors.js';
