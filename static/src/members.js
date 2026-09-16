d5fc05429a652e5005931620a31b85434c6a50f8
Asteria Hart <asteria@strawbs.io>
2026-09-15 21:21:55 -0400

    feat: ring member descriptions, landing page

---
+++ static/src/members.js
@@ -0,0 +1,44 @@
+'use strict';
+
+async function fetchMembers() {
+  const res = await fetch('https://api.ooooo.garden/ring/members', {
+    method: 'GET',
+  });
+
+  if (!res.ok) {
+    console.error('failed to fetch ring members');
+    return;
+  }
+
+  const { members } = await res.json();
+
+  console.log('got members', members);
+
+  const elements = members
+    .map((member) => {
+      console.log('processing member', member);
+      const link = document.createElement('a');
+
+      link.setAttribute('href', member.url);
+      link.textContent = member.title;
+
+      return [link, member.owner, member.description];
+    })
+    .map(([link, owner, description]) => {
+      const item = document.createElement('li');
+      item.append(link);
+      item.append(` - by ${owner}`);
+      item.append(document.createElement('br'));
+      item.append(`↳ ${description}`);
+
+      console.log('have item', item);
+      return item;
+    });
+
+  console.log('elements', elements);
+
+  const list = document.querySelector('#directory');
+  list.append(...elements);
+}
+
+document.addEventListener('DOMContentLoaded', fetchMembers);
